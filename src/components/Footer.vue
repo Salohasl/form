481ffeb2231cb3a04aed794f92177169c98560d0
Maxim Zaividy <m.zavidiy@consult-info.ru>
2024-09-12 17:38:19 +0300

    Добавляет footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="footer">
+    <p>all rights reserved</p>
+  </div>
+</template>
+
+<style scoped>
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    background: #FAFAFA;
+    text-align: center;
+  }
+
+  .footer p {
+    font-weight: 400;
+  }
+</style>
--- src/page/FormPage.vue
+++ src/page/FormPage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue";
 import Form from "../components/Form.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
   <Header />
   <Form />
+  <Footer />
 </template>
 
 <style scoped>
